41692455acfc5cca13c3745092b35b155ed7721b
BrunoMotaBr <87215947+BrunoMotaBr@users.noreply.github.com>
2025-04-23 18:28:26 -0300

    Add files via upload

---
+++ TerceiraAula/operadoresAritimeticos.py
@@ -0,0 +1,20 @@
+#Adição
+print(1 + 3)
+
+#Subtração
+print(3 - 1)
+
+#Multiplicação
+print(3 * 1)
+
+#Divisão
+print(1 / 2)
+
+#Divisão inteira
+print(1 // 2)
+
+#Modulo
+print(10 % 3)
+
+#Exponenciação
+print(2 ** 2)
